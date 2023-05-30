c66c9656445a2a692830f618ec3d35add3464cac
Maksim Rozhkov <kermit@mail.ru>
2023-05-30 22:06:04 +0300

    Добавлен минимальный набор органов управления

--- AndroidApp/app/src/main/java/ru/dors/androidusbcdc/MainActivity.kt
+++ AndroidApp/app/src/main/java/ru/dors/androidusbcdc/MainActivity.kt
@@ -2,10 +2,22 @@ package ru.dors.androidusbcdc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+
+                val message = findViewById<TextView>(R.id.connection_msg)
+                message.text = "Check Everything"
+            }
+        })
     }
 }
